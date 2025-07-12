a5089ef0c1df33d61935e570306d42d107f91271
Nidhi <nidhikatlawar@gmail.com>
2025-07-12 16:45:49 +0530

    nini component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { NidhiComponent } from '../nidhi/nidhi.component';
+import { NiniComponent } from './nini/nini.component';
 
 export const routes: Routes = [
     {
         path:'nidhi',component: NidhiComponent
+    },
+    {
+        path:'nini', component:NiniComponent
     }
 ];
