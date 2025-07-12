1e09b44e169e70fd1ca6d481ae6986574db79e8d
Aastha Khangar <khangar12aastha@gmail.com>
2025-07-12 16:48:50 +0530

    aastha component

---
+++ src/app/aastha/aastha.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aastha',
+  standalone: true,
+  imports: [],
+  templateUrl: './aastha.component.html',
+  styleUrl: './aastha.component.scss'
+})
+export class AasthaComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AasthaComponent } from './aastha/aastha.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'aastha' ,component:AasthaComponent
+    }
+];
