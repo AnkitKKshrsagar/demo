36364e569a37e03326df90c9270c09ad6f9e2bc5
rutika-12 <rutika1185@gmail.com>
2025-07-12 15:58:40 +0530

    rutika component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RutikaComponent } from './rutika/rutika.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'app', component: AppComponent,
+    },
+
+    {
+        path:'rutika', component: RutikaComponent,
+    }
+];
