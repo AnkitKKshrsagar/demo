b7ae569f7803c2e649f0bd62282597fd7a2f4972
Nidhi <nidhikatlawar@gmail.com>
2025-07-12 15:55:50 +0530

    nidhi component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NidhiComponent } from '../nidhi/nidhi.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'nidhi',component: NidhiComponent
+    }
+];
